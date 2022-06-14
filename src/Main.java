55fc471f9d5cf2ae99146300eb8b45c3f8aa02db
zqt <417196284@qq.com>
2022-06-14 15:12:53 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+
+    }
+
+    public void eat(){
+
+        System.out.println("学成归来");
+    }
+
+}
